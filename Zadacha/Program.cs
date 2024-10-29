6d61e22bab53dc2a3402fa39ecc50aa6ecaaf313
1211-3 <1211-3@DESKTOP-PMVUDLD>
2024-10-29 08:40:20 +0200

    Add project files.

---
+++ Zadacha/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread threadA = new Thread(() =>
+            {
+                Console.WriteLine("Nishka A e startirana...");
+                Thread.Sleep(1000);
+                Console.WriteLine("Nishka A prikluchi...");
+            });
+
+            Thread threadZ = new Thread(() =>
+            {
+                Console.WriteLine("Nishka Z e startirana...");
+                Thread.Sleep(1500);
+                Console.WriteLine("Nishka Z prikluchi...");
+            });
+
+            threadA.Start();
+            threadZ.Start();
+
+            Thread dashternaThread = new Thread(() =>
+            {
+                Console.WriteLine("Dashternata nishka e startirana...");
+                Thread.Sleep(2000);
+                Console.WriteLine("Dashternata nishka prikluchi...");
+            });
+            dashternaThread.Start();
+            threadA.Join();
+            threadZ.Join();
+            dashternaThread.Join();
+
+            Console.WriteLine("Vsichki nishki prikluchiha...");
+        }
+    }
+}
